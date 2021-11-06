ac85ab840bab84e7c0deecb20da03a27420da28c
Basil Reji <84758142+basil-reji@users.noreply.github.com>
2021-11-06 22:07:45 +0530

    excel interfacing added

--- Send Messages.py
+++ Send Messages.py
@@ -4,29 +4,32 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+import pandas as pd
 
 # Replace below path with the absolute path
 # to chromedriver in your computer
-driver = webdriver.Chrome('/Downloads/chromedriver.exe')
+driver = webdriver.Chrome('chromedriver.exe')
 
 driver.get("https://web.whatsapp.com/")
 wait = WebDriverWait(driver, 600)
 
-# Replace name with Name of the person
-# or the name of a group
-target = '"Name"'
+df = pd.read_excel("book.xlsx") #path of the excel file goes here
+contacts = df["Contact Name"] #Name of the column curresponding to
+messages = df["Message"] #message content
 
-# Replace the below string with your own message
-string = "Hey!!!"
+n = 3 
 
-x_arg = '//span[contains(@title,' + target + ')]'
-group_title = wait.until(EC.presence_of_element_located((
-	By.XPATH, x_arg)))
-group_title.click()
-inp_xpath = '//div[@class="_13NKt copyable-text selectable-text"][@data-tab="9"]'
-input_box = wait.until(EC.presence_of_element_located((
-	By.XPATH, inp_xpath)))
+for i in range(n):
+	target = ('"{}"'.format(contacts[i]))
+	string = messages[i]
 
-for i in range(10):
+	x_arg = '//span[contains(@title,' + target + ')]'
+	group_title = wait.until(EC.presence_of_element_located((
+		By.XPATH, x_arg)))
+	group_title.click()
+	inp_xpath = '//div[@class="_13NKt copyable-text selectable-text"][@data-tab="9"]'
+	input_box = wait.until(EC.presence_of_element_located((
+		By.XPATH, inp_xpath)))
+
 	input_box.send_keys(string + Keys.ENTER)
 	time.sleep(1)
