35e1c0327c82f6fee8a26f94485fbd46f7ea687f
Basil Reji <84758142+basil-reji@users.noreply.github.com>
2021-11-07 00:30:00 +0530

    image attatchment function added

--- Send Messages.py
+++ Send Messages.py
@@ -17,7 +17,11 @@ df = pd.read_excel("book.xlsx") #path of the excel file goes here
 contacts = df["Contact Name"] #Name of the column curresponding to
 messages = df["Message"] #message content
 
-n = 3 
+imagepath = 'Full path of the image'
+
+delay = 3 #delay in seconds
+
+n = 3 #loop iteration count
 
 for i in range(n):
 	target = ('"{}"'.format(contacts[i]))
@@ -28,8 +32,23 @@ for i in range(n):
 		By.XPATH, x_arg)))
 	group_title.click()
 	inp_xpath = '//div[@class="_13NKt copyable-text selectable-text"][@data-tab="9"]'
+
 	input_box = wait.until(EC.presence_of_element_located((
 		By.XPATH, inp_xpath)))
-
 	input_box.send_keys(string + Keys.ENTER)
-	time.sleep(1)
+
+	attachment_box = driver.find_element_by_xpath('//div[@title = "Attach"]')
+	attachment_box.click()
+
+	image_box = driver.find_element_by_xpath(
+		'//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
+	image_box.send_keys(imagepath)
+
+	time.sleep(3)
+
+	send_button = driver.find_element_by_xpath('//span[@data-icon="send"]')
+	send_button.click()
+
+	time.sleep(delay)
+	search_bar = driver.find_element_by_class_name('_13NKt')
+	search_bar.click()
